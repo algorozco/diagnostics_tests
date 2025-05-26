3e99688770085dada3efea00c04f254b857b69f4
Ana <45442898+algorozco@users.noreply.github.com>
2025-05-26 14:42:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-CAPI-Token, X-Account-ID,x-capi-token,x-account-id");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-CAPI-Token, X-Account-ID, x-capi-token, x-account-id");
   next();
 });
 
